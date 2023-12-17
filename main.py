90429e90d850575867077b6b57f7c1da4d2fef67
ikkai-r <rm.sales92@gmail.com>
2023-12-17 20:05:34 +0800

    left the gui barebones

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 import tkinter.ttk as ttk
-from tkinter import LEFT, BOTTOM
+from tkinter import LEFT, BOTTOM, END
 
 from tktimepicker import SpinTimePickerModern, constants
 
@@ -10,12 +10,57 @@ class AutoJoin(tk.Tk):
         tk.Tk.__init__(self)
         self.style_window()
         self.create_widgets()
-        self.get_time()
+
+    def style_window(self):
+        self.style = ttk.Style(self)
+        self.tk.call('source', 'awthemes-10.4.0/awthemes-10.4.0/awdark.tcl')
+        self.tk.call('::themeutils::setHighlightColor', 'awdark', '#E529EE')
+        self.style.theme_use('awdark')
+        self.geometry("700x400")
+        self.title("auto join zoom meetings .ೃ࿐")
+        self.iconbitmap("icon.ico")
+
+    def create_widgets(self):
+        self.topframe = ttk.Frame(self)
+        self.zoom_label = ttk.Label(self.topframe, text="Zoom Link")
+        self.zoom_entry = ttk.Entry(self.topframe)
+        self.time_label = ttk.Label(self.topframe, text="Time")
+
+        self.button = ttk.Button(self.topframe, text="Submit", command=self.on_button)
+        self.topframe.pack(expand=True, fill="both")
+
+        #zoom
+        self.zoom_label.pack(side=LEFT)
+        self.zoom_entry.pack(side=LEFT)
+
+        #time
+        self.time_label.pack(side=LEFT)
+        self.time_picker = SpinTimePickerModern(self.topframe)
+        self.time_picker.addAll(constants.HOURS24)  # adds hours clock, minutes and period
+        self.time_picker.configureAll(bg="#404040", height=1, fg="#ffffff", hoverbg="#404040",
+                                 clickedbg="#2e2d2d", hovercolor="#DEDBDD", clickedcolor="#ffffff",
+                                 font=("Arial", 20, "bold"))
+        self.time_picker.configure_separator(bg="#404040", fg="#ffffff")
+        self.time_picker.pack(side=LEFT)
+
+        #days
+        self.days_label = ttk.Label(self.topframe, text="Days")
+        self.days_label.pack(side=LEFT)
+        self.list = tk.Listbox(self.topframe, selectmode="multiple")
+        self.list.pack(expand="True")
+
+        x = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
+             "Saturday"]
+
+        for each_item in range(len(x)):
+            self.list.insert(END, x[each_item])
+            self.list.itemconfig(each_item)
+
+        #button
+        self.button.pack(side=LEFT)
 
     def get_time(self):
-        self.botframe = ttk.Frame(self)
-        self.botframe.pack(side=BOTTOM, expand=True, fill="both")
-        time_picker = SpinTimePickerModern(self.botframe)
+        time_picker = SpinTimePickerModern(self)
         time_picker.addAll(constants.HOURS24)  # adds hours clock, minutes and period
         time_picker.configureAll(bg="#404040", height=1, fg="#ffffff", hoverbg="#404040",
                                  clickedbg="#2e2d2d", hovercolor="#DEDBDD", clickedcolor="#ffffff",
@@ -23,27 +68,8 @@ class AutoJoin(tk.Tk):
         time_picker.configure_separator(bg="#404040", fg="#ffffff")
         time_picker.pack()
 
-    def style_window(self):
-        self.style = ttk.Style(self)
-        self.tk.call('source', 'awthemes-10.4.0/awthemes-10.4.0/awdark.tcl')
-        self.tk.call('::themeutils::setHighlightColor', 'awdark', '#E529EE')
-        self.style.theme_use('awdark')
-        self.geometry("500x200")
-        self.title("auto join zoom meetings .ೃ࿐")
-        self.iconbitmap("icon.ico")
-
-    def create_widgets(self):
-        self.topframe = ttk.Frame(self)
-        self.entry = ttk.Entry(self.topframe)
-        self.label = ttk.Label(self.topframe, text="Zoom Link")
-        # self.button = ttk.Button(self.topframe, text="Get", command=self.on_button)
-        self.topframe.pack(expand=True, fill="both")
-        self.label.pack(side=LEFT)
-        self.entry.pack(side=LEFT)
-        self.button.pack(side=LEFT)
-
-    # def on_button(self):
-    # print(self.entry.get())
+    def on_button(self):
+     print(self.zoom_entry.get())
 
 
 window = AutoJoin()
