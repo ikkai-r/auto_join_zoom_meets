cf3bb993a813185e3607c10786fd645542c6b816
ikkai-r <rm.sales92@gmail.com>
2023-12-20 15:13:29 +0800

    added mon - fri

--- main.py
+++ main.py
@@ -16,16 +16,13 @@ def process_meeting():
     # lists for the zoom meetings per day
     mon_meets = []
     tues_meets = []
-
-    # add more if necessary
-    # wed_meets = []
+    thurs_meets = []
+    fri_meets = []
 
     for row in range(1, df1.max_row):
         zoom_meet = ''
         zoom_time = ''
         for col in df1.iter_cols(1, df1.max_column):
-            print(col[row].value)
-            print(col_count)
 
             # get zoom link for the row
             if col_count == 1:
@@ -38,20 +35,29 @@ def process_meeting():
                 col_count += 1
             else:
                 # check what day the zoom meet occurs
-                if col[row].value == 'M':
-                    mon_meets.append(zoom_meet + '|' + str(zoom_time))
-                elif col[row].value == 'T':
-                    tues_meets.append(zoom_meet + '|' + str(zoom_time))
+                meet_days = str(col[row].value).split()
+
+                for mdays in meet_days:
+                    if mdays == 'M':
+                        mon_meets.append(zoom_meet + '|' + str(zoom_time))
+
+                    if mdays == 'T':
+                        tues_meets.append(zoom_meet + '|' + str(zoom_time))
+
+                    if mdays == 'Th':
+                        thurs_meets.append(zoom_meet + '|' + str(zoom_time))
+
+                    if mdays == 'F':
+                        fri_meets.append(zoom_meet + '|' + str(zoom_time))
 
                 # reset col_count
                 col_count = 0
 
-    week_meets = [mon_meets, tues_meets]
+    week_meets = [mon_meets, tues_meets, thurs_meets, fri_meets]
     join_meets(week_meets)
 
 
 def join_meets(week_meets):
-
     # process monday meets
     for mon_meet in week_meets[0]:
         mon_meet_str = mon_meet.split("|")
@@ -66,7 +72,18 @@ def join_meets(week_meets):
         meet_link = tues_meet_str[0]
         sc.every().tuesday.at(meet_time).do(open_meet, meet_link)
 
-    # add more days if necessary
+    for thurs_meet in week_meets[2]:
+        thurs_meet_str = thurs_meet.split("|")
+        meet_time = thurs_meet_str[1]
+        meet_link = thurs_meet_str[0]
+        sc.every().thursday.at(meet_time).do(open_meet, meet_link)
+
+    for fri_meet in week_meets[3]:
+        fri_meet_str = fri_meet.split("|")
+        meet_time = fri_meet_str[1]
+        meet_link = fri_meet_str[0]
+        sc.every().friday.at(meet_time).do(open_meet, meet_link)
+
 
 def open_meet(meet_link):
     wb.open(meet_link)
