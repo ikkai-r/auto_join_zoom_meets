ac0153a459934c2781299a0834d0c37091e3522c
ikkai-r <rm.sales92@gmail.com>
2023-12-20 09:36:25 +0800

    removed gui

--- main.py
+++ main.py
@@ -1,46 +1,12 @@
-import tkinter as tk
-import tkinter.ttk as ttk
-from tkinter import W
 import openpyxl as op
+import webbrowser as wb
 
-class AutoJoin(tk.Tk):
-    def __init__(self):
-        tk.Tk.__init__(self)
+#get the zoom meetings
+dataframe = op.load_workbook("zoom_meets.xlsx")
 
-        self.dataframe = op.load_workbook("zoom_meets.xlsx")
+df1 = dataframe.active
 
-        self.style_window()
-        self.create_widgets()
-        self.display_scheduled()
-
-    def style_window(self):
-        self.style = ttk.Style(self)
-        self.tk.call('source', 'awthemes-10.4.0/awthemes-10.4.0/awdark.tcl')
-        self.tk.call('::themeutils::setHighlightColor', 'awdark', '#E529EE')
-        self.style.theme_use('awdark')
-        self.geometry("700x400")
-        self.title("auto join zoom meetings .ೃ࿐")
-        self.iconbitmap("icon.ico")
-
-    def create_widgets(self):
-        topframe = ttk.Frame(self)
-
-        df1 = self.dataframe.active
-
-        for row in range(0, df1.max_row):
-            for col in df1.iter_cols(1, df1.max_column):
-                print(col[row].value)
-
-        # button
-        pass
-    def on_button(self):
-        # get the info for the zoom and put in zoom entries
-        pass
-
-
-    def display_scheduled(self):
-        pass
-
-
-window = AutoJoin()
-window.mainloop()
+#read the cols and rows
+for row in range(1, df1.max_row):
+    for col in df1.iter_cols(1, df1.max_column):
+        print(col[row].value)
